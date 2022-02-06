6ed6e9dd7b3787700d2ff2dbf7d2832e24364ec1
niconsm <supercuate16@gmail.com>
2022-02-06 03:19:10 -0300

    desafios DOM y Eventos terminado

--- login.js
+++ login.js
@@ -1,27 +1,87 @@
 
-let signin = document.getElementById('login')
+//# Night Theme
 
-signin.submit = () => userType()
+let boton = document.getElementById('button')
 
+boton.onclick = () => nightTh()
 
+function nightTh() {
 
-function userType(event) {
+    let bodynt = document.getElementsByTagName("body")
+    let logo = document.getElementById('logo')
+    let signin = document.getElementById('signin')
+    let signup = document.getElementById('signup')
+    let rememberme = document.querySelector('#remember2')
+    let regHere = document.querySelector('#regHere')
+    let forgetPass = document.querySelector('#forgetPass')
 
-    event.preventDefault()
+    if (boton.value === '0') {
 
-    let form = event.target
+        //Version Nocturna
 
-    let username = document.getElementById('user')
-    let pass = document.getElementById('password')
+        bodynt[0].className = 'bg-zinc-900'
 
-    if ((username === 'admin') && (pass === 'admin')) {
+        logo.src = 'https://raw.githubusercontent.com/niconsm16/Myfit-Machicado/master/src/images/myfitlogo3.webp'
 
-        window.location = '/admin.html';
+        signin.className = signin.className.replace(/(?:^|\s)text-gray-900(?!\S)/g, ' text-gray-100 ')
+        signup.className = signup.className.replace(/(?:^|\s)text-gray-600(?!\S)/g, ' text-gray-300 ')
+        rememberme.className = rememberme.className.replace(/(?:^|\s)text-gray-900(?!\S)/g, ' text-gray-400 ')
+
+        regHere.className = regHere.className.replace(/(?:^|\s)text-green-600(?!\S)/g, ' text-verde ')
+        forgetPass.className = forgetPass.className.replace(/(?:^|\s)text-green-600(?!\S)/g, ' text-verde ')
+
+        let lunita = document.createElement('div')
+        lunita.className = 'lunita'
+
+        document.body.append(lunita)
+
+        boton.innerHTML = 'Volvé a la vista clásica'
+        boton.value = '1'
+
     } else {
 
-        window.location = '/user.html';
+        //Version de Dia
+
+        bodynt[0].className = ''
+
+        logo.src = "https://raw.githubusercontent.com/niconsm16/Myfit-Machicado/master/src/images/myfitlogo.webp"
+
+        signin.className = signin.className.replace(/(?:^|\s)text-gray-100(?!\S)/g, ' text-gray-900 ')
+        signup.className = signup.className.replace(/(?:^|\s)text-gray-300(?!\S)/g, ' text-gray-600 ')
+        rememberme.className = rememberme.className.replace(/(?:^|\s)text-gray-400(?!\S)/g, ' text-gray-900 ')
+
+        regHere.className = regHere.className.replace(/(?:^|\s)text-verde(?!\S)/g, ' text-green-600 ')
+        forgetPass.className = forgetPass.className.replace(/(?:^|\s)text-verde(?!\S)/g, ' text-green-600 ')
+
+        let lunita2 = document.getElementsByClassName('lunita')
+        lunita2[0].remove()
+
+        boton.innerHTML = 'Probá la versión nocturna'
+        boton.value = '0'
     }
 }
 
+//# Login
 
-console.log("Events")
+let b = document.getElementById('username')
+let c = document.getElementById('password')
+
+let a = document.getElementById('submit')
+
+a.onclick = () => userType()
+
+
+
+function userType() {
+
+    if ((b.value == '=admin') && (c.value == 'admin')) {
+
+        window.location = 'https://www.youtube.com/';
+
+    } else {
+
+        window.location = '/pages/user.html';
+
+    }
+}
+
