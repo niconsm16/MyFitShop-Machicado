8fd4bab74f1485d2bf575324aedb1b239bb97295
niconsm <supercuate16@gmail.com>
2022-02-18 16:27:44 -0300

    sweetalert2 in login

--- js/datausers.js
+++ js/datausers.js
@@ -1,13 +1,23 @@
-alert(`USUARIOS:
+Swal.fire({
+    imageUrl: "https://raw.githubusercontent.com/niconsm16/Myfit-Machicado/master/src/images/myfitlogo.webp",
+    imageHeight: 96,
+    imageAlt: "MyFit Shop",
+    html: `<div class="text-black asap">USUARIOS:<br>
+<div style="padding:0.5em 0"><i>user: admin</i><br>
+<i>pass: admin</i></div> 
+    <p style="font-size: 0.8em;">ADMIN: TOTALMENTE FUNCIONAL<br>
+(Borren/creen sin miedo, no pasa nada)</p>
 
-ADMIN: TOTALMENTE FUNCIONAL 
-(Borren/creen sin miedo, no pasa nada)
-user: admin
-pass: admin
+<br>
+USER:<br>
+<div style="padding:0.5em 0"><i>user: cualquier cosa</i><br>
+<i>pass: cualquier cosa</i></div>
+<p style="font-size: 0.8em;">Diseñada primer interfaz<br>
+BUSCADOR TOTALMENTE FUNCIONAL<br>(Resultados parciales/completos)<br>
+Items precargados: gas, jugo, galletas, poncho zarpado, etc.</p>
+`,
 
-USER:
-Diseñada primer interfaz
-BUSCADOR TOTALMENTE FUNCIONAL (Resultados parciales/completos)
-(items precargados: gas, jugo, galletas, poncho zarpado, etc.)
-user: cualquier cosa
-pass: cualquier cosa`)
+    confirmButtonText: '<div style="color:black">Entendido!</div>',
+    confirmButtonColor: '#70d70c',
+    confirmButtonAriaLabel: '',
+})
