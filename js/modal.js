d50e5d70ecf9dd132ee72df714b4e607b243850f
niconsm <supercuate16@gmail.com>
2022-02-22 09:41:11 -0300

    modal tunned

--- js/modal.js
+++ js/modal.js
@@ -2,10 +2,23 @@ const modal = document.getElementById('cartItemsCount')
 modal.onclick = () => {
 
     Swal.fire({
-        title: 'TuCarrito',
+        title: 'TuCarrito <img src="https://raw.githubusercontent.com/niconsm16/Myfit-Machicado/master/src/images/myfitlogo3.webp" width="15%">',
         text: 'Do you want to continue',
-        confirmButtonText: 'Cool',
+        confirmButtonText: 'PAGAR',
+        confirmButtonColor: '#70D70C',
+        showCancelButton: true,
+        cancelButtonText: 'Vaciar',
+        cancelButtonColor: '#FF8000',
+        showDenyButton: true,
+        denyButtonText: 'Volver',
+        denyButtonColor: '#424242',
         backdrop: false,
+        toast: false,
+        color: 'white',
+        background: 'black',
+        position: 'center-end',
+        grow: 'column',
+
     })
 
 }
